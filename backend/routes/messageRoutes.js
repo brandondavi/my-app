4cac52dc788df7d6fc94eeaf14be373443c3bc3f
Brandon Silva <brandondavi123@gmail.com>
2026-05-03 13:52:17 -0700

    Delete backend directory

--- backend/routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { sendMessage } = require('../controllers/messageController');
-const { protect } = require('../middleware/auth');
-
-router.post('/send', protect, sendMessage);
-
-module.exports = router;
